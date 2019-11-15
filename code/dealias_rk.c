613968f7e1dfb9af43d74da44b57f4a21e5504dc
Natalia Vladimirova <nata@tangle.local>
2019-11-14 21:35:01 -0700

    Separated dealiasing from time evolution

---
+++ code/dealias_rk.c
@@ -0,0 +1,116 @@
+#include "header.h"
+
+
+static  fftw_complex  *Psi, *PsiHat;
+
+static  double           dealiasZ;
+static  int              filtersize;
+static  double           *filter;
+
+static  int 		 np, myid;
+static  int 		 N0;
+
+/* ---------------------------------------------------------------- */
+
+
+void dealias_init(geom_ptr geom, fftw_complex *psi, fftw_complex *psihat)
+{
+
+  int     i, j, Nx, Ny, filtersize, Ngrids;
+  double  x;
+
+  Psi = psi;
+  PsiHat = psihat;
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  MPI_Comm_size(MPI_COMM_WORLD, &np);
+
+  N0 =  geom->N0;
+ 
+  dealiasZ   =  geom->dealiasZ;
+  filtersize =  geom->dealiasF;
+  N0 =          geom->N0;
+
+  /*--- set up dealising filter ---*/
+
+  filter = (double *)malloc( filtersize*sizeof(double) );
+
+  for (i=0; i<filtersize; i++){
+    x = 1 - 1.*i/filtersize;
+    filter[i] = x*x*(3-2*x);
+  }
+
+ 
+}
+
+
+
+/* ---------------------------------------------------------------- */
+
+void dealias(int grid){
+
+  int            N, n, i, j, kx, ky, dkx, dky;
+  int            kmin, kmax;
+
+  fftw_complex   psi;
+
+  N = N0 * pow(2,grid);
+
+  if ((dealiasZ <= 0) || (dealiasZ >= 1))  kmin = N/2;
+  else                                     kmin = N/2*dealiasZ;
+  kmax = kmin + filtersize;
+  if (kmax > N/2) kmax = N/2; 
+
+
+  n = N/np;
+ 
+  fft_wrap(Psi, grid, FORWARD);
+ 
+  for (j=0; j<n; j++) for (i=0; i<N; i++) {
+
+    kx = i;
+    ky = j + myid*n;
+
+    if (kx > N/2) kx = kx-N;
+    if (ky > N/2) ky = ky-N;
+
+    if ((abs(kx) < kmax) && (abs(ky) < kmax)) {
+
+      psi[0] = Psi[N*j+i][0];
+      psi[1] = Psi[N*j+i][1];
+ 
+      /*--- smoothing dealiasing ---*/
+
+      dkx = abs(kx) - kmin;
+      dky = abs(ky) - kmin;
+
+      if (dkx >= 0) {
+	psi[0] = psi[0] * filter[dkx];
+	psi[1] = psi[1] * filter[dkx];
+      }
+
+      if (dky >= 0) {
+	psi[0] = psi[0] * filter[dky];
+	psi[1] = psi[1] * filter[dky];
+      }
+
+    } else {
+      psi[0] = 0;
+      psi[1] = 0;
+    }
+
+    Psi[N*j+i][0] = psi[0];
+    Psi[N*j+i][1] = psi[1];
+
+  }
+
+  /*-- save a copy of psihat to work array --*/
+
+  fft_save_fourier(Psi, PsiHat, grid);
+
+  fft_wrap(Psi, grid, BACKWARD);
+
+
+}
+
+/* ---------------------------------------------------------------- */
--- code/evolve_rk.c
+++ code/evolve_rk.c
@@ -1,23 +1,11 @@
 #include "header.h"
 
-static  int 		 myid, np;
-static  int 		 N0;
-static  double           L;
-static  double           pi;
+static  int   myid, np;
 
-static  double           dealiasZ;
-static  int              filtersize;
-static  double           *filter;
+static  int   tmrRK;
 
-
-static  fftw_complex  *Psi, *PsiHat;
-
-
-static  int              tmrRK;
-static  char             msg[80];
-
-
-static void   dealias(int grid);
+extern void   dealias(int grid);
+extern void   dealias_init(geom_ptr geom, fftw_complex *psi, fftw_complex *psihat);
 
 extern void   rk_one_step(int grid, double dt);          // Uo := Uo + dU
 extern void   rk_init(geom_ptr geom, fftw_complex *psi, int np_in);
@@ -29,37 +17,17 @@ void
 evolve_init(geom_ptr geom, phys_ptr phys, 
 	       fftw_complex *psi, fftw_complex *psihat){
 
-  int     i, j, Nx, Ny, filtersize, Ngrids;
-  double  x;
-
-  Psi = psi;
-  PsiHat = psihat;
-
+  
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
   MPI_Comm_size(MPI_COMM_WORLD, &np);
 
-  N0 =  geom->N0;
-  L  =  geom->Lx;
-  pi =  acos(0)*2;
-
-
-  dealiasZ   =  geom->dealiasZ;
-  filtersize =  geom->dealiasF;
-  N0 =          geom->N0;
-
-  /*--- set up dealising filter ---*/
-
-  filter = (double *)malloc( filtersize*sizeof(double) );
-
-  for (i=0; i<filtersize; i++){
-    x = 1 - 1.*i/filtersize;
-    filter[i] = x*x*(3-2*x);
-  }
-
+ 
   /*--- create suppementary arrays ---*/
 
   rk_init(geom, psi, np);
   rhs_init(geom, phys, np); 
+
+  dealias_init(geom, psi, psihat);
 
   
   /*--- timers ---*/
@@ -72,10 +40,8 @@ evolve_init(geom_ptr geom, phys_ptr phys,
 
 void evolve_one_step(int grid, double dt)   // Uo := Uo + dU
 {
-  int N;
 
   timer_on(tmrRK);
-
 
   rk_one_step(grid, dt);
   
@@ -93,70 +59,3 @@ void evolve_biglin(int grid,  double dt){
 
 /* ---------------------------------------------------------------- */
 
-void dealias(int grid){
-
-  int            N, n, i, j, kx, ky, dkx, dky;
-  int            kmin, kmax;
-
-  fftw_complex   psi;
-
-  N = N0 * pow(2,grid);
-
-  if ((dealiasZ <= 0) || (dealiasZ >= 1))  kmin = N/2;
-  else                                     kmin = N/2*dealiasZ;
-  kmax = kmin + filtersize;
-  if (kmax > N/2) kmax = N/2; 
-
-
-  n = N/np;
- 
-  fft_wrap(Psi, grid, FORWARD);
- 
-  for (j=0; j<n; j++) for (i=0; i<N; i++) {
-
-    kx = i;
-    ky = j + myid*n;
-
-    if (kx > N/2) kx = kx-N;
-    if (ky > N/2) ky = ky-N;
-
-    if ((abs(kx) < kmax) && (abs(ky) < kmax)) {
-
-      psi[0] = Psi[N*j+i][0];
-      psi[1] = Psi[N*j+i][1];
- 
-      /*--- smoothing dealiasing ---*/
-
-      dkx = abs(kx) - kmin;
-      dky = abs(ky) - kmin;
-
-      if (dkx >= 0) {
-	psi[0] = psi[0] * filter[dkx];
-	psi[1] = psi[1] * filter[dkx];
-      }
-
-      if (dky >= 0) {
-	psi[0] = psi[0] * filter[dky];
-	psi[1] = psi[1] * filter[dky];
-      }
-
-    } else {
-      psi[0] = 0;
-      psi[1] = 0;
-    }
-
-    Psi[N*j+i][0] = psi[0];
-    Psi[N*j+i][1] = psi[1];
-
-  }
-
-  /*-- save a copy of psihat to work array --*/
-
-  fft_save_fourier(Psi, PsiHat, grid);
-
-  fft_wrap(Psi, grid, BACKWARD);
-
-
-}
-
-/* ---------------------------------------------------------------- */
