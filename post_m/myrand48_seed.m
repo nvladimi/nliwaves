6f8d51e02118e2ebece4be5492caca7294c1d603
Natalia Vladimirova <nvladimirova@gmail.com>
2020-02-20 19:50:51 -0700

    Matlab script to generate initial random seeds

---
+++ post_m/myrand48_seed.m
@@ -0,0 +1,29 @@
+function myrand48_seed(fname, np, seed)
+
+  jmax = 100;
+  
+  rand ("twister", seed);
+
+  j = rand(np*3, 1); 
+  j = fix(j*jmax)+1;
+
+  ind = zeros(np*3,1);
+  ind(1) = j(1);
+
+  for i=2:np*3
+      ind(i) = ind(i-1) + j(i);
+  end
+  
+  x = rand(ind(end), 1);
+  x = x(ind);
+
+  xmax = 2^16 - 1;
+  y = fix(x*xmax); 
+  z = uint16(y);
+
+  fid = fopen(fname, 'wb');
+  fwrite(fid, z, 'uint16');
+  fclose(fid);
+  
+end
+
