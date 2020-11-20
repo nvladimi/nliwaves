f61de9a2583e390a030da5b5f11d1e03415b8ba9
Natalia Vladimirova <nvladimirova@gmail.com>
2020-11-20 13:08:54 +0200

    adding script for randomizing phases in NLS/capillary restart files

---
+++ post_m/rerand_psi.m
@@ -0,0 +1,41 @@
+function rerand_psi(fname_in, fname_out, N, seed, randomness)
+%
+%   rerand_psi(fname_in, fname_out, N, randomness)
+%
+%   Input:
+%
+%       fname_in:   binary input file (size N)
+%       fname_out:  binary output file (size N)
+%       randomness: [0,1)
+%
+
+  fid = fopen(fname_in, 'rb');
+  p = fread(fid, N*N*2, 'double');
+  fclose(fid);
+
+  p = reshape(p,2,N,N);
+  p = p(1,:,:)+ 1i*p(2,:,:);
+  p = reshape(p,N,N);
+
+
+  rand ("twister", seed);
+
+  r = (rand(N,N)*2 - 1)*pi*randomness;
+
+  fp = fft2(p);
+  fp = fp .* exp(1i*r);
+  pnew = ifft2(fp);
+
+  p  = zeros(2,N,N);
+  p(1,:,:) = real(pnew);
+  p(2,:,:) = imag(pnew);
+
+  p=reshape(p,N*N*2,1);
+
+  fid = fopen(fname_out, 'wb');
+  fwrite(fid, p, 'double');
+  fclose(fid);
+
+
+end
+
